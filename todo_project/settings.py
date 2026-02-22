c2ce2393ec13e02eb5d10a05af35c58355ec3438
Sadman25 <shadman.saqif@gmail.com>
2026-02-23 01:08:05 +0600

    CSRF origin added after domain generation

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -42,6 +42,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://subclimatic-sherri-nonprovocatively.ngrok-free.dev',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'https://ostadbatch-10-module-2-production.up.railway.app'
 ]
 
 
